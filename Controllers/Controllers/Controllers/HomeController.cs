294c16c20181150d12146a263e139bd1b8eacba8
Eduardo Olimpio <106188291+eduardoolimpiodev@users.noreply.github.com>
2022-12-13 09:52:50 -0300

    Add Code
    Code

---
+++ Controllers/Controllers/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers.Controllers
+{   
+    // Usa-se a rota vazia apenas em uma controller geralmente na controller inicial.
+    [Route("")]
+    [Route("gestao")]
+    public class HomeController : Controller
+    {
+        //Todas as rotas dessa controller agora ficam gestao/Index, gestao/Privacy, etc...
+        // Para passar um parâmetro não existente em uma rota: 
+        ///Home/Index/10?categorias=Teste
+        //ou /Home/Index/10?categorias=Teste&categoria2=Teste2 caso haja um segundo parâmetro.
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(int id, Guid categoria)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/Controllers/Startup.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            //Adiciona o MVC ao Pipeline, o MVC é um middleware, depois configura o MVC lá em baixo.
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            // Aqui se configura o MVC.
+            app.UseMvc(routes =>
+            {
+                //routes.MapRoute(
+                //    name: "modulos",
+                //    template: "Gestao/{controller=Home}/{action=Index}/{id?}");
+
+                //routes.MapRoute(
+                //    name: "categoria",
+                //    template: "{controller=Home}/{action=Index}/{id}/{categoria?}");
+
+                //A rota padrão sempre deverá ser a última a ser especificada.
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
